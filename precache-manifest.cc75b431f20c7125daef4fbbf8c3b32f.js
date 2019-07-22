e064d5d5d93935445ebc906fdc29893bbbcb6d46
Dmitriy <kaishien777@gmail.com>
2019-07-22 06:17:26 +0300

    Updates

---
+++ precache-manifest.cc75b431f20c7125daef4fbbf8c3b32f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a54871cc0beb77138801fcd0b522d2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17d2475f2e10b65c9663",
+    "url": "./static/css/main.66fcaff5.chunk.css"
+  },
+  {
+    "revision": "eb984728082a98bd5414",
+    "url": "./static/js/2.99e130ca.chunk.js"
+  },
+  {
+    "revision": "17d2475f2e10b65c9663",
+    "url": "./static/js/main.c59be39d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
